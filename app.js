bcd565f23246abe8ebd106d5ba7119f2493da46a
Bruno De Luca <bruno.deluca@ing.austral.edu.ar>
2020-04-25 18:23:19 -0300

    install atob

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const AWS = require("aws-sdk");
+const atob = require('atob');
 
-const [sk, ak] = atob(process.env.A).split(":");
-
-AWS.config.update({
-    region: 'us-east-1',
-    accessKeyId: ak,
-    secretAccessKey: sk
-});
+if (process.env.NODE_ENV === 'production') {
+    const [sk, ak] = atob(process.env.A).split(":");
+    
+    AWS.config.update({
+        region: 'us-east-1',
+        accessKeyId: ak,
+        secretAccessKey: sk
+    });
+}
+else {
+    AWS.config.update({
+        region: 'us-east-1'
+    });
+}
 
 const app = express();
 
